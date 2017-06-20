004f0ea604e2351f52318b09d3f048b18f5af68a
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2017-06-20 14:54:24 -0400

    Exercise for Random, inclusive exclusive

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -2,22 +2,31 @@ import java.util.Random;
 
 
 public class RandomGenerator{
-    public static void main(String[] args)
-    {
-        output("Generate 10 random integers between 0 and 6");
-        Random rnd = new Random();
-        
-        for (int i = 1; i <= 10; ++i)
-        {
-          int randomInt = 1 + rnd.nextInt(6);
-          output("Generated number: " + randomInt);
-        }
-    
-        output("Done.");
-    }
-  
-  private static void output(String aMessage)
-  {
-    System.out.println(aMessage);
-  }
+	public static void main(String[] args)
+	{
+		//output("Generate 10 random integers between 0 and 6");
+		Random rnd = new Random();
+
+
+		for (int i = 1; i <= 10; ++i){
+			// 1 to 6 inclusive
+			//int randomInt = 1 + rnd.nextInt(6);
+			
+			//between inclusive 5 to 95 exclusive
+			int randomInt = 5 + rnd.nextInt(90);
+			output("Generated number: " + randomInt);
+		}
+//
+//		int temp = (5+rnd.nextInt(90));
+//		while( temp != 5){
+//			System.out.println(temp);
+//			temp = (5+rnd.nextInt(90));
+//		}
+		output("Done.");
+	}
+
+	private static void output(String aMessage)
+	{
+		System.out.println(aMessage);
+	}
 }
